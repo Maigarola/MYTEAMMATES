d182eb0594b72f4d822b2554ad10f7e2704f2c71
Maigarola <maigarola@gmail.com>
2019-09-30 13:27:35 +0200

    my3commit

--- main.js
+++ main.js
@@ -15,8 +15,6 @@ fetch(url, {}).then(function (response) {
 
     let myteammates = data.people;
 
-    console.log(myteammates[0]);
-    
     printtable(myteammates);
 
     let table_body = document.getElementById("t_body");
@@ -27,24 +25,40 @@ fetch(url, {}).then(function (response) {
 
         for (let i = 0; i < lista.length; i++) {
             template += `
-        <tr>
-            <td> ${lista[i].name}</td>
-            <td> ${lista[i].age}</td>
-            <td> ${lista[i].role}</td>
-            <td> ${lista[i].team}</td>
-            <td> ${lista[i].seniority}</td>
+            <tr>
+                <td> ${lista[i].name}</td>
+                <td> ${lista[i].age}</td>
+                <td> ${lista[i].role}</td>
+                <td> ${lista[i].team}</td>
+                <td> ${lista[i].seniority}</td>
+                ${window(i,lista)}
+            </tr>`;
 
-            <td data-fancybox data-options='{"src": "#exampleModal", "touch": false, "smallBtn" : false}' href="javascript:;" "><button class="button">More info</button></td>
-            
-            <div style="display:none; max-width:500px;" id="exampleModal">
-            <img src="${lista[i].name}" alt="contact"> 
-            </div>
-
-        </tr>
-        `;
             t_body.innerHTML = template;
         }
 
+    }
+
+    function window(p, lista) {
+
+        let template = "";
+        template += `
+        <td data-fancybox data-options= '{"src": "#exampleModal", "smallBtn" : false}' href="javascript:;"><button class="button">More info</button></td>
+            
+        <div class = "window" id="exampleModal">
+            ${lista[p].name}
+            <h5>${lista[p].name}</h5>
+            <img src="${lista[p].contact_info.photo}" alt="photo" height = "200px"> 
+            <h6><span>NickName</span> ${lista[p].contact_info.nickName} </p>
+            <p><span>Phone</span> ${lista[p].contact_info.phone} </p>
+            <p><span>Site</span> <a href=${lista[p].contact_info.site}> ${lista[p].contact_info.site} </a></p>
+            <p><span>Contact</span> <button class="e_button" href= "mailto:${lista[p].contact_info.email}">Send me a mail</button></p>
+
+            <p><button data-fancybox-close class="button">Close</button></p>
+            </div>
+            `
+            return template;
+            
     }
 
 }).catch(function (error) {
