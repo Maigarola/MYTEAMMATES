aac88942564a1faf7714a22fcc2fa01273914990
Maigarola <maigarola@gmail.com>
2019-10-01 00:10:56 +0200

    withsearch

--- main.js
+++ main.js
@@ -4,10 +4,10 @@
 let url = "https://api.myjson.com/bins/adpvt";
 
 fetch(url, {}).then(function (response) {
-    if (response.ok) {
-        return response.json();
-    }
-    throw new Error(response.statusText);
+if (response.ok) {
+    return response.json();
+}
+throw new Error(response.statusText);
 
 }).then(function (json) {
 
@@ -20,11 +20,12 @@ fetch(url, {}).then(function (response) {
 
     //filters creation
 
+    // checkboxes
     let checkroles = howmanyroles(myteammates);
 
     for (let i = 0; i < checkroles.length; i++) {
         let div = document.createElement("div");
-        div.setAttribute("class","mycheckboxs");
+        div.setAttribute("class", "mycheckboxs");
         let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.id = checkroles[i].replace(/\s/g, "_");
@@ -35,18 +36,45 @@ fetch(url, {}).then(function (response) {
         herecheckfilters.append(div);
         div.append(checkbox);
         div.append(label);
-
     }
 
-    printtable(myteammates);
+    //search
+    let search = document.createElement("input");
+    search.type = "search";
+    search.placeholder = "Search...";
+    search.id = "tsearch";
+    let bsearch = document.createElement("button");
+    bsearch.setAttribute("id", "bsearch");
+    bsearch.append(document.createTextNode("Search!"));
 
+    heresearchfilters.append(search);
+    heresearchfilters.append(bsearch);
 
-    function printtable(lista) {
+    document.getElementById("bsearch").addEventListener("click", () => searchfilter(myteammates));
 
-        let template = " ";
+    searchfilter();
 
-        for (let i = 0; i < lista.length; i++) {
-            template += `
+    function searchfilter() {
+        document.getElementById("t_body").innerHTML = "";
+        let sfiltered = [];
+        let s = document.getElementById("tsearch").value;
+        console.log(s);
+
+        for (let i = 0; i < myteammates.length; i++) {
+            if (myteammates[i].name.includes(s) || myteammates[i].contact_info.nickName.includes(s)) {
+                sfiltered.push(myteammates[i]);
+            }
+        }
+        printtable(sfiltered);
+
+}
+
+function printtable(lista) {
+
+    let template = " ";
+
+    for (let i = 0; i < lista.length; i++) {
+        template += `
             <tr>
                 <td> ${lista[i].name}</td>
                 <td> ${lista[i].age}</td>
@@ -56,16 +84,16 @@ fetch(url, {}).then(function (response) {
                 ${window(lista[i])} 
             </tr>`;
 
-            table_body.innerHTML = template;
-        }
-
+        table_body.innerHTML = template;
     }
 
-    function window(element) {
+}
 
-        let template = "";
-        ournick = (element.contact_info.nickName).replace(/\s/g, "_");
-        template += `
+function window(element) {
+
+    let template = "";
+    ournick = (element.contact_info.nickName).replace(/\s/g, "_");
+    template += `
         <td ><button class="button" data-fancybox data-options='{"src":"#${ournick}","touch": false, "smallBtn" : false}' href="javascript:;">More info</button>            
         <div class = "thewindow" id= "${ournick}">
             <h3>${element.name}</h3>
@@ -75,33 +103,33 @@ fetch(url, {}).then(function (response) {
             <p><span>Site</span> <a href=${element.contact_info.site}> ${element.contact_info.site} </a></p>
             <p><span>Contact</span>`
 
-        if (element.contact_info.email == null) {
-            template += ` We don't have any contact info`
-        } else {
-            template += ` <button class="e_button"><a id="mail" href="mailto:${element.contact_info.email}" target="_top">Send me a mail</button></a></p>`
-        }
+    if (element.contact_info.email == null) {
+        template += ` We don't have any contact info`
+    } else {
+        template += ` <button class="e_button"><a id="mail" href="mailto:${element.contact_info.email}" target="_top">Send me a mail</button></a></p>`
+    }
 
-        template += `
+    template += `
             <p><button data-fancybox-close  class="c_button">Close</button></p>
             </div>
             </td>`
 
-        return template;
+    return template;
 
-    }
+}
 
-    function howmanyroles(lista) {
+function howmanyroles(lista) {
 
-        let roles = [];
+    let roles = [];
 
-        for (let i = 0; i < lista.length; i++) {
-            if (!roles.includes(lista[i].role)) {
-                roles.push(lista[i].role);
-            }
+    for (let i = 0; i < lista.length; i++) {
+        if (!roles.includes(lista[i].role)) {
+            roles.push(lista[i].role);
         }
-        roles.sort();
-        return roles;
     }
+    roles.sort();
+    return roles;
+}
 
 
 }).catch(function (error) {
