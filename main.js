5a41f3ed6335efcad73bbd95d7877818a260f034
Maigarola <maigarola@gmail.com>
2019-09-30 23:27:23 +0200

    checkroles

--- main.js
+++ main.js
@@ -15,8 +15,31 @@ fetch(url, {}).then(function (response) {
 
     let myteammates = data.people;
     let table_body = document.getElementById("t_body");
+    let herecheckfilters = document.getElementById("checkroles");
+    let heresearchfilters = document.getElementById("searching");
+
+    //filters creation
+
+    let checkroles = howmanyroles(myteammates);
+
+    for (let i = 0; i < checkroles.length; i++) {
+        let div = document.createElement("div");
+        div.setAttribute("class","mycheckboxs");
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = checkroles[i].replace(/\s/g, "_");
+
+        let label = document.createElement("label");
+        label.htmlFor = checkroles[i].replace(/\s/g, "_");
+        label.append(document.createTextNode(checkroles[i]));
+        herecheckfilters.append(div);
+        div.append(checkbox);
+        div.append(label);
+
+    }
 
     printtable(myteammates);
+
 
     function printtable(lista) {
 
@@ -30,7 +53,7 @@ fetch(url, {}).then(function (response) {
                 <td> ${lista[i].role}</td>
                 <td> ${lista[i].team}</td>
                 <td> ${lista[i].seniority}</td>
-                ${window(lista[i])}
+                ${window(lista[i])} 
             </tr>`;
 
             table_body.innerHTML = template;
@@ -46,18 +69,38 @@ fetch(url, {}).then(function (response) {
         <td ><button class="button" data-fancybox data-options='{"src":"#${ournick}","touch": false, "smallBtn" : false}' href="javascript:;">More info</button>            
         <div class = "thewindow" id= "${ournick}">
             <h3>${element.name}</h3>
-            <img src="${element.contact_info.photo}" alt="photo" height = "200px"> 
+            <img src="${element.contact_info.photo}" alt="photo"> 
             <p><span>NickName</span> ${element.contact_info.nickName} </p>
             <p><span>Phone</span> ${element.contact_info.phone} </p>
             <p><span>Site</span> <a href=${element.contact_info.site}> ${element.contact_info.site} </a></p>
-            <p><span>Contact</span> <button class="e_button"><a id="mail" href="mailto:${element.contact_info.email}" target="_top">Send me a mail</button></a></p>
-            
+            <p><span>Contact</span>`
+
+        if (element.contact_info.email == null) {
+            template += ` We don't have any contact info`
+        } else {
+            template += ` <button class="e_button"><a id="mail" href="mailto:${element.contact_info.email}" target="_top">Send me a mail</button></a></p>`
+        }
+
+        template += `
             <p><button data-fancybox-close  class="c_button">Close</button></p>
             </div>
-            </td>
-            `
+            </td>`
+
         return template;
 
+    }
+
+    function howmanyroles(lista) {
+
+        let roles = [];
+
+        for (let i = 0; i < lista.length; i++) {
+            if (!roles.includes(lista[i].role)) {
+                roles.push(lista[i].role);
+            }
+        }
+        roles.sort();
+        return roles;
     }
 
 
